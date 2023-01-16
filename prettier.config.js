c818ee42afc6a2b30f1944fa4cb2fa69ec92b3c3
ohjuhyeon <dhwngus32@naver.com>
2023-01-16 23:49:39 +0900

    study 0116

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// https://prettier.io/docs/en/options.html
+
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+};
--- src/App.js
+++ src/App.js
@@ -1,79 +1,67 @@
 /*eslint-disable */
 
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  let [title, setTitle] = useState([
-    "남자 코트 추천",
-    "강남 우동 맛집",
-    "파이썬독학",
-  ]);
-  let [describe, setDescribe] = useState("2월 17일 발행");
+  const [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학']);
+  const [describe, setDescribe] = useState('2월 17일 발행');
+  const [modal, setModal] = useState(false);
 
   //state를 변경하고싶을땐 setButtonCount함수를 사용하여 state 변경한다.
   //useState(0); <- 0으로 초기값 초기값 설정
-  let [buttonCount, setButtonCount] = useState(0);
+  const [buttonCount, setButtonCount] = useState([0, 0, 0]);
 
   return (
-    <div className="App">
-      <div className="black-nav">
+    <div className='App'>
+      <div className='black-nav'>
         <h4>ReactBlog</h4>
       </div>
 
-      <button
-        onClick={() => {
-          let copy = [...title];
-          copy.sort();
-          setTitle(copy);
-        }}
-      >
-        정렬 버튼
-      </button>
-
-      <button
-        onClick={() => {
-          // ...문법은 괄호를 다시 벗겨주세요 (화살표를 다시 설정)
-          let copy = [...title];
-          copy[0] = "여자 코트 추천";
-          setTitle(copy);
-        }}
-      >
-        이름변경 버튼
-      </button>
-      <div className="list">
-        <h4>
-          {title[0]}
-          <span
-            onClick={() => {
-              setButtonCount(buttonCount + 1);
-            }}
-          >
-            👍
-          </span>
-          {buttonCount}
-        </h4>
-        <p>{describe}</p>
-      </div>
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>{describe}</p>
-      </div>
-      <div className="list">
-        <h4>{title[2]}</h4>
-        <p>{describe}</p>
-      </div>
-      <Modal />
+      {title.map(function (a, i) {
+        return (
+          <div className='list'>
+            <h4
+              onClick={() => {
+                modal == true ? setModal(false) : setModal(true);
+              }}
+            >
+              {title[i]}
+              <span
+                onClick={() => {
+                  const copy = [...buttonCount];
+                  copy[i] = copy[i] + 1;
+                  setButtonCount(copy);
+                }}
+              >
+                👍
+              </span>
+              {buttonCount[i]}
+            </h4>
+            <p>{describe}</p>
+          </div>
+        );
+      })}
+      {modal == true ? <Modal title={title} setTitle={setTitle} /> : null}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
-    <div className="modal">
-      <h4>제목</h4>
+    <div className='modal'>
+      <h4>{props.title[0]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button
+        onClick={() => {
+          const copy = [...props.title];
+          copy[0] = '여자 코트 추천';
+          props.setTitle(copy);
+        }}
+      >
+        글수정
+      </button>
     </div>
   );
 }
